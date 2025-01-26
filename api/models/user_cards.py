d18336c7fa2af2bcb56f317bb12e8aa691cd9fdf
lucienlaumont <lucienlaumont36@gmail.com>
2025-01-26 23:25:55 +0100

    Delete project

--- api/models/user_cards.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Table, Column, Integer, Boolean, ForeignKey
-from .base import Base
-
-user_cards = Table(
-    'user_cards', Base.metadata,
-    Column('user_id', Integer, ForeignKey('users.id'), primary_key=True),
-    Column('card_id', Integer, ForeignKey('cards.id'), primary_key=True),
-    Column('is_offer', Boolean, nullable=False)  # True si la carte est offerte, False si recherchée
-)
--- src/bot/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Charger les variables d'environnement
-load_dotenv()
-
-# Variables globales
-BOT_TOKEN = os.getenv("DISCORD_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
