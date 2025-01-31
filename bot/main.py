eb4a1a9d29c7bc898ea2c25f48374c015afef950
lucienlaumont <lucienlaumont36@gmail.com>
2025-01-31 12:43:51 +0100

    Delete previous file

--- bot/main.py
+++
@@ -1,8 +0,0 @@
-import os
-from discord.ext import commands
-from bot.commands import bot
-
-TOKEN = os.getenv("DISCORD_BOT_TOKEN")  # Ajoutez votre token ici ou dans un fichier .env
-
-if __name__ == "__main__":
-    bot.run(TOKEN)
